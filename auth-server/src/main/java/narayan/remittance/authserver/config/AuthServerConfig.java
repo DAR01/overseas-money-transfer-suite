eaa8cdde00700796f3907430c41879a42a4720f2
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-10 10:07:53 +0530

    updated

--- auth-server/src/main/java/narayan/remittance/authserver/config/AuthServerConfig.java
+++ auth-server/src/main/java/narayan/remittance/authserver/config/AuthServerConfig.java
@@ -1,11 +1,18 @@
 package narayan.remittance.authserver.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -16,11 +23,40 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authManager);
+		endpoints.tokenStore(tokenStore(null));// Persist the tokens in the database
 	}
 
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("permitAll()");
+	}
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.jdbc(dataSource());
+	}
+
+	@Bean
+	public TokenStore tokenStore(DataSource dataSource) {
+		return new JdbcTokenStore(dataSource);
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+		dataSource.setUrl("jdbc:hsqldb:hsql://localhost/remittance");
+		dataSource.setUsername("SA");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	/*
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("remittance").secret("secret").authorizedGrantTypes("password")
 				.scopes("read,write,trust");
-	}
+	}*/
+	
+	
 }
